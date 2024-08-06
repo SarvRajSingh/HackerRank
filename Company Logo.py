3d34284f5ad2d29156e7558dc8925cb9c37e0229
SarvRajSingh <109565327+SarvRajSingh@users.noreply.github.com>
2024-08-06 16:57:27 +0530

    Create Company Logo.py
    A newly opened multinational brand has decided to base their company logo on the three most common characters in the company name. They are now trying out various combinations of company names and logos based on this condition. Given a string , which is the company name in lowercase letters, your task is to find the top three most common characters in the string.
    
    - Print the three most common characters along with their occurrence count.
    - Sort in descending order of occurrence count.
    - If the occurrence count is the same, sort the characters in alphabetical order.

---
+++ Company Logo.py
@@ -0,0 +1,24 @@
+from collections import Counter
+import re
+
+def count_all_chars(string):
+    # filter all non-alphabets
+    string = re.sub(r'[^a-zA-Z]', "", string)
+    char_count= Counter(string)
+    #instead of using 're' we can use the below code block also
+    #char_count = Counter(char for char in string if char.isalpha())
+    return char_count # it will return a Counter datatype
+    
+
+# Test the function
+string = "Hello, World!"
+char_counts = count_all_chars(string)
+top_3_chars = char_counts.most_common() # it will return a list enclosing tuples
+
+# Sort characters with the same count in descending order of their ASCII values
+top_3_chars.sort(key=lambda x: (-x[1], ord(x[0])))
+
+for i, (char, count) in enumerate(top_3_chars):
+    if i >= 3:
+        break
+    print(f"'{char}': {count}")
